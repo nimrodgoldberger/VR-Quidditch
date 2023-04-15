4ef374e44154c94ce6b78d378d0475c71f066e5d
Eden Benzimra <88041964+edenbenzimra@users.noreply.github.com>
2023-04-15 10:37:37 +0300

    moving broom up and down

---
+++ Assets/Scripts/ActivateVoice.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Meta.WitAi;
+using UnityEngine.InputSystem;
+
+
+public class ActivateVoice : MonoBehaviour
+{
+    [SerializeField]
+    private Wit wit;
+
+    private void Update()
+    {
+        if (wit == null)
+            wit = GetComponent<Wit>();
+    }
+    public void TriggerPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            WitActivate();
+    }
+    public void WitActivate()
+    {
+        wit.Activate();
+    }
+}
