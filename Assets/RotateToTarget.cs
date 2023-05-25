680900ba774358f29bf8b5b7b957de80042acbf3
DanAmoyal <dan2737@gmail.com>
2023-05-25 17:21:05 +0300

    flight

---
+++ Assets/RotateToTarget.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class RotateToTarget : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    [SerializeField] public float rotationSpeed = 5f;
+    [SerializeField] public Camera childCamera;
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            // Calculate the rotation for the parent object
+            Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles.x, target.eulerAngles.y, transform.eulerAngles.z);
+
+            float diff = transform.eulerAngles.y - target.eulerAngles.y;
+            if (Mathf.Approximately(diff, 0f))
+                return;
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            
+            if (childCamera != null)
+            {
+                childCamera.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
+}
