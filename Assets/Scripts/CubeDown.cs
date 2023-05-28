61c500348caa715493c4928104c2e594454dbe7e
Eden Benzimra <88041964+edenbenzimra@users.noreply.github.com>
2023-05-29 00:31:20 +0300

    rapidus in progress

--- Assets/Scripts/CubeDown.cs
+++ Assets/Scripts/CubeDown.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine;
+using UnityEngine.UI;
 
 //IMPORTANT : activate the voice commands using VR pushing button, it is explained in https://www.youtube.com/watch?v=SJ96P-ZhBoc&t=213s.
 
@@ -16,6 +18,7 @@ using UnityEngine;
 
 public class CubeDown : MonoBehaviour
 {
+    public Text displayText; // Reference to the UI Text component
 
     /////////////////////////////////////////////////////////////////////////////////////////
     //private bool appVoiceActive = false; //for the Keyboard activation try
@@ -30,7 +33,7 @@ public class CubeDown : MonoBehaviour
     {
         float distance = 1.5f;  // distance we want the cube to move
         float duration = 0.5f;  //how long the cube should take to travel this distance
-        
+
         switch (directionString[0]) //first word in the phrase recognized in the voice command. (here we only have one word)
         {
             case "up":
@@ -40,7 +43,31 @@ public class CubeDown : MonoBehaviour
                 StartCoroutine(MoveCube(Vector3.down * distance, duration));//Vector3.down is (0,-1,0) //the coroutine function here is MoveCube.
                 //In Unity, a coroutine is a function that can run alongside other functions in your game. When you call a coroutine function, it will start running and will continue running until it reaches a yield statement. At that point, the coroutine function will pause and allow other code to run, and then it will resume from where it left off on the next frame.
                 break;
+            case "fast":
+                StartCoroutine(SpeedBoost(duration * 2));
+                break;
         }
+    }
+
+    private IEnumerator SpeedBoost(float duration)
+    {
+        Debug.Log("In SpeedBoost!"); // TEMPORARY
+        float elapsedTime = 0.0f;
+        float boost = 1.5f;
+
+        //cubes[0].transform.position = Vector3.up * 2f;// TEST
+
+        //displayText.text = "Hello, world!"; //FOR COOL DOWN!!!! Sets the text value of the UI Text component 
+
+        cubes[0].GetComponent<ActionBasedContinuousMoveProvider>().moveSpeed *= boost;
+        while (elapsedTime < duration) //move for "duration" seconds
+        {
+            elapsedTime += Time.deltaTime; //Time.deltaTime is the time in seconds it took for the last frame to be rendered. it creates a smooth animationand the speed is independent from frame rate so it will be identical on all machines.
+        }
+
+        cubes[0].GetComponent<ActionBasedContinuousMoveProvider>().moveSpeed = cubes[0].GetComponent<ActionBasedContinuousMoveProvider>().moveSpeed / boost;
+
+        yield return null;
     }
 
     private IEnumerator MoveCube(Vector3 direction, float duration) //IEnumerator is the return value of a coroutine function. //A coroutine is a function that can be paused and resumed at any point during its execution.
@@ -64,7 +91,7 @@ public class CubeDown : MonoBehaviour
 
 
     ////Tryout to activate using keyboard
-    
+
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //
     //private void Awake()
