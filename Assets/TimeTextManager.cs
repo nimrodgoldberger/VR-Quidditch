d08d1e9558dac07545068527b92aad9bee03eb65
Nimrod <nimrodgl@mta.ac.il>
2023-08-06 14:24:18 +0300

    Timer runs backwards
    + added option to activate method when timer reaches 0

--- Assets/TimeTextManager.cs
+++ Assets/TimeTextManager.cs
@@ -5,21 +5,36 @@ using TMPro;
 public class TimeTextManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float currentTime = 0f;
+    [SerializeField] private float timeRemaining = 120.0f;
+    private bool timerIsRunning = false;
     private TMP_Text timerText;
 
     void Start()
     {
         // Get reference to the Text component for displaying the timer
         timerText = GetComponent<TMP_Text>();
+        timerIsRunning = true;
     }
 
     void Update()
     {
-        currentTime += Time.deltaTime;
-        int minutes = Mathf.FloorToInt(currentTime / 60f);
-        int seconds = Mathf.FloorToInt(currentTime % 60f);
+        if(timerIsRunning)
+        {
+            if(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
 
-        timerText.text = string.Format("Time: {0:00}:{1:00}", minutes, seconds);
+            int minutes = Mathf.FloorToInt(timeRemaining / 60f);
+            int seconds = Mathf.FloorToInt(timeRemaining % 60f);
+            timerText.text = string.Format("Time: {0:00}:{1:00}", minutes, seconds);
+        }
     }
 }
