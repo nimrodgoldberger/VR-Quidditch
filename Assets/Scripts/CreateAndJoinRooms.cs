15b2d0c47bf58de9ba095801d279dfc096400d20
DanAmoyal <dan2737@gmail.com>
2023-08-14 01:58:55 +0300

    online playable

--- Assets/Scripts/CreateAndJoinRooms.cs
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -22,6 +22,6 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        PhotonNetwork.LoadLevel("test");
+        PhotonNetwork.LoadLevel("Online");
     }
 }
